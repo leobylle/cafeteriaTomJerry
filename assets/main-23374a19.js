ad9274f725b09b43956b82eef2359599041174a3
leandro <bylle.leandro@gmail.com>
2023-10-14 17:00:46 -0300

    Updates

--- assets/main-23374a19.js
+++ assets/main-23374a19.js
@@ -1,4 +1,4 @@
-import{c as n}from"./utilidades-85c577d0.js";import{a as r,i as d,r as i,b as s}from"./menuCarrinho-3dec26cb.js";function c(){for(const o of n){const e=`<div class='border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group ${o.bebida?"bebida":"acompanhamento"}' id="card-produto-${o.id}">
+import{c as n}from"./utilidades-6105eef8.js";import{a as r,i as d,r as i,b as s}from"./menuCarrinho-fefade57.js";function c(){for(const o of n){const e=`<div class='border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group ${o.bebida?"bebida":"acompanhamento"}' id="card-produto-${o.id}">
         <img
         src="./assets/img/${o.imagem}"
         alt="Cafeteria"
